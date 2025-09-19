85d9bdd44e9db17c436e3e3414d4da6e13b06215
andreauxue <andreauxue@ciencias.unam.mx>
2025-09-18 22:40:10 -0600

    Clase 06

--- Clase 05/adopta_amigo/adopta_amigo/urls.py
+++ Clase 05/adopta_amigo/adopta_amigo/urls.py
@@ -19,10 +19,14 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
+from rest_framework.routers import DefaultRouter 
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    
     
